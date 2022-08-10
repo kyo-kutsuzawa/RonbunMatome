1dcc91bfb4a2b5d221c28e1e88499afb834b3d7e
kyo-kutsuzawa <k.kutsuzawa.430@gmail.com>
2022-08-10 13:32:27 +0900

    Change the name

--- Untei/MainWindow.xaml.cs
+++ Untei/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Untei
+namespace RonbunMatome
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
