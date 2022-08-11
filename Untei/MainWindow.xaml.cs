46f8bd61ea90cadf57637415117f934f75e42c3d
kyo-kutsuzawa <k.kutsuzawa.430@gmail.com>
2022-08-11 09:09:13 +0900

    Implement a tab close function

--- Untei/MainWindow.xaml.cs
+++ Untei/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -36,6 +37,11 @@ namespace RonbunMatome
             selectedId = "";
         }
 
+        /// <summary>
+        /// Show details and comments of the selected item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void BiblioListViewItem_ShowDetails(object sender, MouseButtonEventArgs e)
         {
             BibItem? item = ((ListViewItem)sender).Content as BibItem;
@@ -46,13 +52,16 @@ namespace RonbunMatome
             }
 
             selectedId = item.Id;
+            //PaperTitleBox.Text = item.Title;
             CommentBox.Text = item.Comment;
         }
 
         void BiblioListViewItem_OpenPdf(object sender, MouseButtonEventArgs e)
         {
+            // Get the selected item
             BibItem? item = ((ListViewItem)sender).Content as BibItem;
 
+            // Return if no item is selected
             if (item == null)
             {
                 return;
@@ -68,29 +77,57 @@ namespace RonbunMatome
             string tabHeader = item.Title;
             const int tabHeaderLength = 20;
 
+            // Limit the text length of the tab header
             if (tabHeader.Length > tabHeaderLength)
             {
-                tabHeader = tabHeader.Substring(0, tabHeaderLength - 3) + "...";
+                tabHeader = string.Concat(tabHeader.AsSpan(0, tabHeaderLength - 3), "...");
             }
 
             // Create a PDF browser
             WebBrowser pdfBrowser = new WebBrowser();
             pdfBrowser.Navigate("file://" + item.Files[0] + "#toolbar=1");
-            Grid contentGrid = new Grid();
+            Grid contentGrid = new();
             contentGrid.Children.Add(pdfBrowser);
 
             // Create a new tab
-            TabItem newTabItem = new TabItem
-            {
-                Header = tabHeader,
-                Content = contentGrid
-            };
+            TabItem newTabItem = new();
+            newTabItem.Content = contentGrid;
             BiblioTabControl.Items.Add(newTabItem);
+
+            // Create a tab header
+            ClosableTabHeader header = new ClosableTabHeader();
+            header.Title.Content = tabHeader;
+            header.CloseButton.Click += new RoutedEventHandler(ClosePaperTab);
+            newTabItem.Header = header;
 
             // Change selected tab
             BiblioTabControl.SelectedIndex = BiblioTabControl.Items.Count - 1;
             BiblioTabControl.SelectedItem = newTabItem;
             newTabItem.IsSelected = true;
+        }
+
+        /// <summary>
+        /// Close a tab
+        /// </summary>
+        /// <param name="sender">This must be a Button in a tab header.</param>
+        /// <param name="e"></param>
+        void ClosePaperTab(object sender, RoutedEventArgs e)
+        {
+            // Get the parent tab
+            TabItem? clickedTab = (TabItem?)((ClosableTabHeader)((Grid)((Button)sender).Parent).Parent).Parent;
+
+            if (clickedTab == null)
+            {
+                return;
+            }
+
+            if (clickedTab.Parent != BiblioTabControl)
+            {
+                return;
+            }
+
+            // Remove the clicked tab
+            BiblioTabControl.Items.Remove(clickedTab);
         }
     }
 }
