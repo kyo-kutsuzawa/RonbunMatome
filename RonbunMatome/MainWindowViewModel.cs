52ba37733c349574f52f17f590e43b94d6d3fdb6
Kyo Kutsuzawa <kutsuzawa@tohoku.ac.jp>
2023-03-12 21:44:29 +0900

    Edit

--- RonbunMatome/MainWindowViewModel.cs
+++ RonbunMatome/MainWindowViewModel.cs
@@ -23,6 +23,10 @@ namespace RonbunMatome
         /// </summary>
         public List<BibItem> DisplayedBibList{ get; private set; }
 
+        public AddBibItemCommand AddBibItemCommand { get; private set; }
+        public SaveBibListCommand SaveBibListCommand { get; private set; }
+        public ExportBibListCommand ExportBibListCommand { get; private set; }
+
         /// <summary>
         /// 選択中の文献データ
         /// </summary>
@@ -48,10 +52,10 @@ namespace RonbunMatome
             bibManager = new BibManager();
 
             DisplayedBibList = bibManager.BibList;
-
             TagList = bibManager.ExtractTags();
-
             SelectedBibItem = new();
+
+            ExportBibListCommand = new(this);
         }
 
         /// <summary>
@@ -67,6 +71,105 @@ namespace RonbunMatome
 
         public void OpenPdf(BibItem item)
         {
+        }
+
+        public void ExportToBibTex()
+        {
+            bibManager.ExportToBibtex("library.bib");
+        }
+    }
+
+    internal class AddBibItemCommand : ICommand
+    {
+        private MainWindowViewModel Vm { get; set; }
+
+        public AddBibItemCommand(MainWindowViewModel vm)
+        {
+            Vm = vm;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+        }
+    }
+
+    internal class SaveBibListCommand : ICommand
+    {
+        private MainWindowViewModel Vm { get; set; }
+
+        public SaveBibListCommand(MainWindowViewModel vm)
+        {
+            Vm = vm;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+        }
+    }
+
+    internal class ExportBibListCommand : ICommand
+    {
+        private MainWindowViewModel Vm { get; set; }
+
+        public ExportBibListCommand(MainWindowViewModel vm)
+        {
+            Vm = vm;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            Vm.ExportToBibTex();
         }
     }
 }
